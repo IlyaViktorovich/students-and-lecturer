b31ba3c22d8ef67b00d657fdbc888b46431ff200
IlyaViktorovich <giv123@mail.ru>
2021-12-11 05:25:57 +0300

    free

--- students.py
+++ students.py
@@ -9,6 +9,15 @@ class Student:
  
     def add_courses(self, course_name):
         self.finished_course.append(course_name)   
+
+    def rate_lectur(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_attached and course in lecturer.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
  
      
 class Mentor:
@@ -16,7 +25,14 @@ class Mentor:
         self.name = name
         self.surname = surname
         self.courses_attached = []
-        
+
+class Lecturer(Mentor):
+    def __init__(self, courses_attached):
+        self.courses_attached = []
+        self.grades = {}
+
+class Reviewer(Mentor):
+
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -25,12 +41,6 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
-
-class Lecturer(Mentor):
-  pass
-
-class Reviewer(Mentor):
-  pass
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
